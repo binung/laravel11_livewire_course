5d94039f58b8508c61ec24ff6822d692fef98a4f
binung <SG.binung715@gmail.com>
2024-06-01 22:03:01 +0200

    tailwnd css

--- resources/views/livewire/posts/create-post.blade.php
+++ resources/views/livewire/posts/create-post.blade.php
@@ -1,8 +1,15 @@
-<div>
+<div class="px-5">
     <form wire:submit="save">
-        <input type="text" wire:model="title">
-        <textarea wire:model="content"></textarea>
-        <button type="submit">Save</button>
-        <button type="button" wire:click="$commit">Refresh</button>
+        <div class="my-5">
+            <input class="w-64" type="text" wire:model="title">
+        </div>
+        <div class="mb-10 w-64">
+            <textarea class=" w-64" wire:model="content"></textarea>
+        </div>
+        <div>
+            <button class="border border-red-500 py-2 px-3 mb-2" type="submit">Save</button>
+            <button class="border border-x-blue-600 py-2 px-3 mb-2" type="button" wire:click="$commit">Refresh</button>
+            <button class="border border-b-violet-950 py-2 px-3 mb-2" type="button" x-on:click="$wire.$refresh">Alpine Refresh</button>
+        </div>
     </form>
 </div>
